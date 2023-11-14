07a6fd82962b24ac4da80879548081541232d733
AbhayGopal <122857188+AbhayGopal@users.noreply.github.com>
2023-11-15 00:16:27 +0530

    fixed api -ngrok

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ logging.debug("Started")
 
 st.title("Your AI Counselor")
 
-openai.api_base = "http://localhost:1234/v1"  # point to the local server
+openai.api_base = "https://abc123.ngrok.io/v1"  # point to the local server
 openai.api_key = ""  # no need for an API key
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "TheBloke/codellama-13b-instruct.Q5_K_M.gguf"
